81e3a668334798564a42cdc83a26e5f64a54519a
Mario HD <mariohdpz@gmail.com>
2019-06-14 22:52:28 -0500

    Solve exercise 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
+import { createStore } from  'redux';
+import { Provider } from 'react-redux';
+
+const initialState = "Hola mundo!"
+
+const reducer = (state=initialState, action) => {
+  /** Aquí podriamos hacer algo dependiendo de la acción */
+  return state
+}
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <HelloWorld />
-    </div>
+    </Provider>
   );
 }
 
--- src/components/HelloWorld/index.js
+++ src/components/HelloWorld/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { connect } from "react-redux"
 
-const HelloWorld = props => <h1 className="main">Hello world!</h1>;
+const HelloWorld = props => <h1 className="main">{props.message}</h1>;
 
-export default HelloWorld;
+const mapStateToProps = reduxState => ({
+    message: reduxState,
+});
+
+export default connect(mapStateToProps)(HelloWorld);
